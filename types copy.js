d71a2db48ddb6dc9a48bff0ecb33f8fb30afa6c0
sanjidanahar <cse02707453@gmail.com>
2024-02-25 01:23:57 -0800

    js type added

---
+++ types copy.js
@@ -0,0 +1,18 @@
+// int a = 5;
+// string b = 'Sanjida Nahar';
+// bool c = true, false;
+// object students = { name: 'noya' id: 56};  
+// int []  numbers = [12, 15, 20]
+// string[] friends = ['abul', 'babul'];
+
+// dynamic type language
+const a =  5;
+const b = 'Samsu kopai na ekhon ar';
+const ages = [5, 8, 15, 9];
+const students = {id: 26, class: 9};
+// console.log(typeof a, typeof b, tySpeof ages, typeof students);
+
+
+let x = 5;
+let y = x;
+console.log()
